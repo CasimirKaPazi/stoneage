f967b9ed742be4d7dee233cca93fa4198abab020
Casimir <jul.lutz@gmx.net>
2014-02-10 17:12:02 +0100

    support moretrees and use override_item

--- overwrite.lua
+++ overwrite.lua
@@ -1,53 +1,47 @@
 --
--- overwrite default nodes and tools
+-- overwrite nodes and tools
 --
 
--- stoneage = {}
-local entity
+minetest.override_item("default:axe_wood", {
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=0,
+		groupcaps={
+		}
+	}
+})
+	
+minetest.override_item("default:pick_wood", {
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=0,
+		groupcaps={
+		}
+	}
+})
 
--- registered
-local registered = function(case,name)
-	local params = {}
-	local list
-	if case == "item" then list = minetest.registered_items end
-	if case == "node" then list = minetest.registered_nodes end
-	if case == "craftitem" then list = minetest.registered_craftitems end
-	if case == "tool" then list = minetest.registered_tools end
-	if case == "entity" then list = minetest.registered_entities end
-	if list then
-		for k,v in pairs(list[name]) do
-			params[k] = v
-		end
+local trees = {
+	"default:tree",
+	"default:jungletree",
+	"moretrees:beech_trunk",
+	"moretrees:apple_tree_trunk",
+	"moretrees:oak_trunk",
+	"moretrees:sequoia_trunk",
+	"moretrees:birch_trunk",
+	"moretrees:palm_trunk",
+	"moretrees:spruce_trunk",
+	"moretrees:pine_trunk",
+	"moretrees:willow_trunk",
+	"moretrees:rubber_tree_trunk",
+	"moretrees:jungletree_trunk",
+	"moretrees:fir_trunk",
+	"conifers:trunk",
+	"conifers:trunk_reversed",
+}
+
+for _,name in ipairs(trees) do
+	if minetest.registered_items[name] then
+		groups = {tree=1,choppy=2,flammable=2}
+		minetest.override_item(name, {groups = groups})
 	end
-	return params
 end
-
--- tree
-entity = registered("node","default:tree")
-entity.groups = {tree=1,choppy=2,flammable=2}
-minetest.register_node(":default:tree", entity)
-
--- jungletree
-entity = registered("node","default:jungletree")
-entity.groups = {tree=1,choppy=2,flammable=2}
-minetest.register_node(":default:jungletree", entity)
-
--- wooden axe
-entity = registered("tool","default:axe_wood")
-entity.tool_capabilities = {
-			max_drop_level=0,
-			groupcaps={
-				choppy={times={[3]=0.80}, uses=10, maxlevel=1},
-				fleshy={times={[2]=1.50, [3]=0.80}, uses=10, maxlevel=1},
-			}
-		}
-minetest.register_tool(":default:axe_wood", entity)
-
--- wooden pickse
-entity = registered("tool","default:pick_wood")
-entity.tool_capabilities = {
-			max_drop_level=0,
-			groupcaps={
-			}
-		}
-minetest.register_tool(":default:pick_wood", entity)
